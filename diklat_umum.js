bc7ef8b8176f2c6801c5b6f3a8f6112fdb457449
unknown <farrelikhlas@gmail.com>
2024-11-13 19:57:26 +0700

    first commit

---
+++ diklat_umum.js
@@ -0,0 +1,20 @@
+//console. log("HELLO");
+
+//var buah="jeruk";
+//var kota="pontianak";
+//console. log(buah,kota);
+
+//a=20;
+//b=35;
+//c=a*b;
+//console. log(c);
+
+// nama="farrel";
+// jurusan="Arsitek";
+// angkatan="2018";
+// console. log(nama, jurusan, angkatan);
+
+a=500;
+b=850;
+c=a/b;
+console. log(c);
---
+++ diklatumum2.js
@@ -0,0 +1,28 @@
+//Operasi Aritmatika
+// let a=15;
+// let b=4;
+
+// console. log("penjumlahan:", a+b);
+// console. log("pengurangan:", a-b);
+// console. log("perkalian:", a*b);
+// console. log("pembagian:", a/b);
+
+//Operasi Perbandingan
+// let x=30;
+// let y=40;
+
+// console. log("x lebih besar dari y:", x>y);
+// console. log("x lebih kecil dari y:", x<y);
+
+// Logical Operator
+//  let harilibur=true;
+//  let punyatugas=false;
+
+//  if(harilibur && !punyatugas) {
+//      console. log("saya bisa bersantai.");
+//  } else if (harilibur || !punyatugas) {
+//      console. log("saya harus menyelesaikan pekerjaan rumah atau hari ini bukan akhir pekan");
+//  } else {
+//      console. log("hari biasa, ada tugas harus diselesaikan");
+// }
+
